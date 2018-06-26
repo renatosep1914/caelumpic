f66fefb07a2a141f0f33fbbda8206ebca6a774c8
Francisco Renato Gomes Sousa <f.renato.gomes.sousa@avanade.com>
2018-06-25 22:53:34 -0300

    aula 25-06

---
+++ src/app/foto/foto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'foto',
+    template: `<img src="assets/img/ava.png" alt="">`,
+    styles: []
+})
+export class FotoComponent {
+
+}
